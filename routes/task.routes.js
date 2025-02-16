d638bd6a611d5db274bb391461b632073631b393
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-02-16 22:32:24 +0100

    Updated the endpoint that creates the tasks for the week to fetch group members from the DB

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -7,8 +7,13 @@ const Group = require("../models/Group.model");
 
 async function getRecurringTasks(groupId){
     return Group.findById(groupId)
-      .select('recurringTasks -_id')
-      .then(response => response.recurringTasks) //implicit return
+      .select('recurringTasks members -_id')
+      .then(response => {
+        return {
+         recurringTasks: response.recurringTasks,
+         members: response.members
+        }
+      })
       .catch((error) => {
         console.error("Error while fetching recurring tasks ->", error);
         res.status(500).json({ error: "Failed to create the week" });
@@ -27,38 +32,44 @@ router.post("/single", (req, res) => {
 router.post("/week", async (req, res) => {
     const { groupId,   prevWeekNumber } = req.body;
 
-    //Fetch recurring tasks
-    let recurringTasks = await getRecurringTasks(groupId);
+    //Fetch recurring tasks and group members
+    let groupInfo = await getRecurringTasks(groupId);
+    const recurringTasks = groupInfo.recurringTasks;
+    const groupMembers = groupInfo.members;
         
     //Create the tasks
-    if(recurringTasks.length === 0){
+    if (recurringTasks.length === 0) {
       console.error("No recurring tasks found");
       res.status(404).json({ error: "No recurrent tasks found" });
       //TODO - Should the frontend check if the group has any recurring tasks to decide whether to even provided the option to create a week?
     } else {
-        const newTasks = recurringTasks.map((taskName) =>{
-            return {
-              name: taskName,
-              assignee: "67b08d45c85d4d61a35489bf",
-              //TODO create function to randomly assign
-              //Decide how to get group members. Saved in context?
-              group: groupId,
-              weekNumber: prevWeekNumber + 1,
-            };
-        })
+      const newTasks = recurringTasks.map((taskName) => {
+        return {
+          name: taskName,
+          assignee: "67b08d45c85d4d61a35489bf",
+          //TODO create function to randomly assign
+          //Decide how to get group members. Saved in context?
+          group: groupId,
+          weekNumber: prevWeekNumber + 1,
+        };
+      });
 
-    //TODO Assign the tasks
+      //TODO Assign the tasks
 
-    
-    //Save the tasks in the DB
-    Task.insertMany(newTasks)
-    .then((createdTasks) => res.json(createdTasks))
-    .catch((error) => {
-        console.error("Error while creating the tasks for the week ->", error);
-        res.status(500).json({ error: "Failed to create the tasks" });
-    });
-}
-    //Update prevWeekNumber in group (current+1) and in context?
+      //Save the tasks in the DB
+      Task.insertMany(newTasks)
+        .then((createdTasks) => res.json(createdTasks))
+        .catch((error) => {
+          console.error(
+            "Error while creating the tasks for the week ->",
+            error
+          );
+          res.status(500).json({ error: "Failed to create the tasks" });
+        });
+    }
+
+    //Update prevWeekNumber in group (current+1)
+    //The value saved in the context would need to be updated too
 });
 
 //Get task information
