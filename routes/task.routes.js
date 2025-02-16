7a7441fbc326d34e196f356fd60d636b0dbec67c
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-02-16 22:17:59 +0100

    Tidied up very slightly

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -25,45 +25,40 @@ router.post("/single", (req, res) => {
 
 //Create tasks for the week
 router.post("/week", async (req, res) => {
-  const { groupId, prevWeekNumber } = req.body;
+    const { groupId,   prevWeekNumber } = req.body;
 
-  //Fetch recurring tasks
-  let recurringTasks = [];
-  try {
-    recurringTasks = await getRecurringTasks(groupId);
-  } catch {
-    console.error("Error while fetching recurring tasks ->", error);
-    res.status(500).json({ error: "Failed to create the week" });
-  }
-
-  //Create the tasks
-  if (recurringTasks.length === 0) {
-    console.error("No recurring tasks found");
-    res.status(404).json({ error: "No recurrent tasks found" });
-    //TODO - Should the frontend check if the group has any recurring tasks to decide whether to even provided the option to create a week?
-  } else {
-    const newTasks = recurringTasks.map((taskName) => {
-      return {
-        name: taskName,
-        assignee: "67b08d45c85d4d61a35489bf",
-        //TODO create function to randomly assign
-        //Decide how to get group members. Saved in context?
-        group: groupId,
-        prevWeekNumber: prevWeekNumber + 1,
-      };
-    });
+    //Fetch recurring tasks
+    let recurringTasks = await getRecurringTasks(groupId);
+        
+    //Create the tasks
+    if(recurringTasks.length === 0){
+      console.error("No recurring tasks found");
+      res.status(404).json({ error: "No recurrent tasks found" });
+      //TODO - Should the frontend check if the group has any recurring tasks to decide whether to even provided the option to create a week?
+    } else {
+        const newTasks = recurringTasks.map((taskName) =>{
+            return {
+              name: taskName,
+              assignee: "67b08d45c85d4d61a35489bf",
+              //TODO create function to randomly assign
+              //Decide how to get group members. Saved in context?
+              group: groupId,
+              weekNumber: prevWeekNumber + 1,
+            };
+        })
 
     //TODO Assign the tasks
 
+    
     //Save the tasks in the DB
     Task.insertMany(newTasks)
-      .then((createdTasks) => res.json(createdTasks))
-      .catch((error) => {
+    .then((createdTasks) => res.json(createdTasks))
+    .catch((error) => {
         console.error("Error while creating the tasks for the week ->", error);
         res.status(500).json({ error: "Failed to create the tasks" });
-      });
-  }
-  //Update prevWeekNumber in group (current+1) and in context?
+    });
+}
+    //Update prevWeekNumber in group (current+1) and in context?
 });
 
 //Get task information
