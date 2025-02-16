fd7165f26065861a0f4fe591a4fe32a0e2a6da5e
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-02-16 21:59:57 +0100

    Working on the endpoint that creates the tasks for the week

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -2,10 +2,68 @@ const express = require("express");
 const router = express.Router();
 
 const Task = require("../models/Task.model");
+const Group = require("../models/Group.model");
 
-//Create a new task
-router.post("/", (req, res) => {
 
+async function getRecurringTasks(groupId){
+    return Group.findById(groupId)
+      .select('recurringTasks -_id')
+      .then(response => response.recurringTasks) //implicit return
+      .catch((error) => {
+        console.error("Error while fetching recurring tasks ->", error);
+        res.status(500).json({ error: "Failed to create the week" });
+      });
+}
+
+/* //Create a new task
+router.post("/single", (req, res) => {
+    const {name, groupId, weekNumber} = req.body;
+    Task.create({name, group: groupId, weekNumber})
+    .then()
+    .catch()
+}); */
+
+//Create tasks for the week
+router.post("/week", async (req, res) => {
+  const { groupId, prevWeekNumber } = req.body;
+
+  //Fetch recurring tasks
+  let recurringTasks = [];
+  try {
+    recurringTasks = await getRecurringTasks(groupId);
+  } catch {
+    console.error("Error while fetching recurring tasks ->", error);
+    res.status(500).json({ error: "Failed to create the week" });
+  }
+
+  //Create the tasks
+  if (recurringTasks.length === 0) {
+    console.error("No recurring tasks found");
+    res.status(404).json({ error: "No recurrent tasks found" });
+    //TODO - Should the frontend check if the group has any recurring tasks to decide whether to even provided the option to create a week?
+  } else {
+    const newTasks = recurringTasks.map((taskName) => {
+      return {
+        name: taskName,
+        assignee: "67b08d45c85d4d61a35489bf",
+        //TODO create function to randomly assign
+        //Decide how to get group members. Saved in context?
+        group: groupId,
+        prevWeekNumber: prevWeekNumber + 1,
+      };
+    });
+
+    //TODO Assign the tasks
+
+    //Save the tasks in the DB
+    Task.insertMany(newTasks)
+      .then((createdTasks) => res.json(createdTasks))
+      .catch((error) => {
+        console.error("Error while creating the tasks for the week ->", error);
+        res.status(500).json({ error: "Failed to create the tasks" });
+      });
+  }
+  //Update prevWeekNumber in group (current+1) and in context?
 });
 
 //Get task information
