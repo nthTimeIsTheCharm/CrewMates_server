78293f62c90f3fe6a4eb2972f8ba3c29948492a2
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-02-19 20:42:43 +0100

    Added an end date for the week to the group model

--- models/Group.model.js
+++ models/Group.model.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const groupSchema = new Schema({
-  name: { type: String, required: true },
-  members: { 
-    type: [Schema.Types.ObjectId],
-    ref: "User" },
-  recurringTasks: { type:[String], default: ["Takeout the trash", "Wash dishes"]},
-  weekNumber: { type: Number, default: 0}
-},
-{
+const groupSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    members: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    recurringTasks: { type: [String] },
+    weekNumber: { 
+      type: Number, 
+      default: 0 },
+    weekEndDate: { 
+      type: Date, 
+      default: null 
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 module.exports = model("Group", groupSchema);
