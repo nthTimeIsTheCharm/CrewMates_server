c3d522cef8fb1fc5506e337927b93cdb031b937f
Airam86 <satodi86@gmail.com>
2025-03-01 11:05:29 +0100

    added group to token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -63,10 +63,10 @@ router.post("/signup", (req, res, next) => {
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, name, _id, group} = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email, name, _id, group };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
