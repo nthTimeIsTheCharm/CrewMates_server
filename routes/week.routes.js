cb1777409f79d68f42903f20118417f67ded2539
Andrea Bronzoni <bronzoni.andrea@libero.it>
2025-03-01 14:19:17 +0100

    updated wee.routes.js with adjustments for the handling of week creation in a group

--- routes/week.routes.js
+++ routes/week.routes.js
@@ -4,50 +4,66 @@ const router = express.Router();
 //Business logic functions
 const businessLogic = require("../utils/businessLogic");
 const dbCalls = require("../utils/callsToDatabase");
-
 //ROUTES
 
 //Gets the tasks for the current week, if there's a current week
-  //The route expects the currentDate param to be provided as a number
-  //const originalDate = new Date ('YYYY-MM-DD');
-  //const dateAsNumbers = originalDate.getTime();
+//The route expects the currentDate param to be provided as a number
+//const originalDate = new Date ('YYYY-MM-DD');
+//const dateAsNumbers = originalDate.getTime();
 
-  router.get("/:groupId/:currentDate", (req, res, next) => {
+router.get("/:groupId/:currentDate", (req, res, next) => {
   const { groupId, currentDate } = req.params;
 
   //Request weekNumber and weekEndDate to the DB
-  dbCalls.getGroupInfo(groupId, next)
+  dbCalls
+    .getGroupInfo(groupId, next)
     .then((response) => {
-      
       //Check the requester is a group member
-      const isMember = businessLogic.checkMembership( req.payload._id, response.members );
-      if (!isMember){ 
-        res.status(401).json({message: "Sorry, you're not authorized to perform this action."}) 
+      const isMember = businessLogic.checkMembership(
+        req.payload._id,
+        response.members
+      );
+      if (!isMember) {
+        res
+          .status(401)
+          .json({
+            message: "Sorry, you're not authorized to perform this action.",
+          });
         return;
       }
 
-      //If there's an active week, returns the tasks for the week. If not, null.
+      //If there's an active week, returns the tasks for the week. If there is no active week, returns the info to know if the week can be created"
       const weekNumber = response.weekNumber;
       console.log(weekNumber);
       const weekEndDate = response.weekEndDate;
-      if (businessLogic.groupHasActiveWeek(currentDate, weekEndDate) === false ) {
-        res.json("null");
+      const hasActiveWeek = businessLogic.groupHasActiveWeek(
+        currentDate,
+        weekEndDate
+      );
+      if (!hasActiveWeek && !response.recurringTasks.length) {
+        res.json({ hasActiveWeek: false, hasRecurringTasks: false });
+      } else if (!hasActiveWeek && response.recurringTasks.length) {
+        res.json({ hasActiveWeek: false, hasRecurringTasks: true });
       } else {
-        dbCalls.getThisWeeksTasks(groupId, weekNumber, next)
-        .then((thisWeeksTasks) => {
-          res.json(thisWeeksTasks);
-        }).catch((error)=>{
-          next(error);
-        })
+        dbCalls
+          .getThisWeeksTasks(groupId, weekNumber, next)
+          .then((thisWeeksTasks) => {
+            res.json(thisWeeksTasks);
+          })
+          .catch((error) => {
+            next(error);
+          });
       }
     })
-    .catch((error) => { next(error) });
+    .catch((error) => {
+      next(error);
+    });
 });
 
 //Creates tasks for the week, if there isn't an ongoing week
-  //The route expects the currentDate param to be provided as a number
-  //const originalDate = new Date ('YYYY-MM-DD');
-  //const dateAsNumbers = originalDate.getTime();
+//The route expects the currentDate param to be provided as a number
+//const originalDate = new Date ('YYYY-MM-DD');
+//const dateAsNumbers = originalDate.getTime();
 
 router.post("/:groupId/:currentDate", (req, res, next) => {
   const { groupId, currentDate } = req.params;
@@ -63,11 +79,9 @@ router.post("/:groupId/:currentDate", (req, res, next) => {
       );
 
       if (!isMember) {
-        res
-          .status(401)
-          .json({
-            message: "Sorry, you're not authorized to perform this action.",
-          });
+        res.status(401).json({
+          message: "Sorry, you're not authorized to perform this action.",
+        });
         return;
       }
 
@@ -102,24 +116,27 @@ router.post("/:groupId/:currentDate", (req, res, next) => {
           .then((savedTasks) => {
             response.tasks = savedTasks;
             //Updated the group in the DB with the new week number and end date
-          }).then(() => {
+          })
+          .then(() => {
             dbCalls.updateWeekEndDateAndNumber(
               groupId,
               weekNumber + 1,
               businessLogic.calculateEndDate(currentDate)
             );
-          }).then((updatedWeekInfo) => {
+          })
+          .then((updatedWeekInfo) => {
             response.newWeek = updatedWeekInfo;
             //Send back a response
             res.status(200).json(response);
-          }).catch((error) => {
+          })
+          .catch((error) => {
             next(error);
           });
       }
     })
     .catch((error) => {
       next(error);
-    }); 
+    });
 });
 
 module.exports = router;
